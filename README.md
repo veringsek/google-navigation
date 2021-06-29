c0f11236d378f2b5096b0fff1933d7df52ba4522
veringsek <veringsek@gmail.com>
2021-06-29 20:49:33 +0800

    basic functions

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# google-navigation
-A set of hotkeys to navigate through Google Search. 
+# Google Navigation
+Google Navigation is a Chrome extension which brings a set of hotkeys for users to navigate through Google Search page. 
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
+
+Copyright © ROC 110 (2021), veringsek
---
+++ main.js
@@ -0,0 +1,23 @@
+let button = document.createElement('div');
+button.classList.add('google-navigation--button');
+
+let gs = [...document.getElementsByClassName('g')];
+for (let [i, g] of gs.entries()) {
+    let num = i + 1;
+
+    let cloned = button.cloneNode();
+    cloned.innerHTML = num;
+    cloned.classList.add(`google-navigation--button-${num}`);
+
+    let h3 = g.getElementsByTagName('h3')[0]; 
+    h3.insertBefore(cloned, h3.children[0]);
+}
+
+document.body.addEventListener('keypress', function (ev) {
+    if (ev.target.tagName !== 'INPUT') {
+        let button = document.getElementsByClassName(`google-navigation--button-${ev.key}`)[0];
+        if (button) {
+            button.click();
+        }
+    }
+});
