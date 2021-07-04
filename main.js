71638127724fc4a27ead538340b73ed664dd1568
veringsek <veringsek@gmail.com>
2021-07-04 12:41:22 +0800

    the first hotkey is Enter now

--- main.js
+++ main.js
@@ -14,13 +14,19 @@ function getClonedButton(key, string) {
 function plantButtons() {
     let gs = [...document.getElementById('search').getElementsByClassName('g')];
     for (let [i, g] of gs.entries()) {
-        let num = i + 1;
+        let num = i;
+        let cloned;
+        if (num === 0) {
+            cloned = getClonedButton('Enter', '⮨');
+        } else {
+            cloned = getClonedButton(num); 
+        }
         let h3 = g.getElementsByTagName('h3')[0];
-        h3.insertBefore(getClonedButton(num), h3.children[0]);
+        h3.insertBefore(cloned, h3.children[0]);
     }
 
     let wikiWholepage = document.getElementsByClassName('kp-wholepage')[0]; 
-    let wikiTitle = [...wikiWholepage.getElementsByTagName('h2')].filter(h2 => h2.offsetParent !== null)[0];
+    // let wikiTitle = [...wikiWholepage.getElementsByTagName('h2')].filter(h2 => h2.offsetParent !== null)[0];
     let wikiContent = document.getElementById('kp-wp-tab-cont-overview');
     if (wikiWholepage) {
         let cloned = getClonedButton('w', 'W');
